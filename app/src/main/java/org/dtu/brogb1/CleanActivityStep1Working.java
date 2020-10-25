5cdaee1798f9cc89fd71065e94abc1bcfc06f46d
Folkmann <theme1256@gmail.com>
2020-10-25 14:30:20 +0100

    Clean-sider lavet som enkelte activities, burde nok være fragments

---
+++ app/src/main/java/org/dtu/brogb1/CleanActivityStep1Working.java
@@ -0,0 +1,14 @@
+package org.dtu.brogb1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CleanActivityStep1Working extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clean_step1_working);
+    }
+}
