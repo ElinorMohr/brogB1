7fad74a79c0bae5bef8314020a77fcccac2885b8
Kristoffer <ktis10@hotmail.com>
2020-10-25 16:05:12 +0100

    Igang med FragmentBrygMenu

--- app/src/main/java/org/dtu/brogb1/StartSide.java
+++ app/src/main/java/org/dtu/brogb1/StartSide.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class StartSide extends AppCompatActivity {
+public class StartSide extends AppCompatActivity implements View.OnClickListener{
     Button visit;
+    ImageButton settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,35 +21,35 @@ public class StartSide extends AppCompatActivity {
         Button clean = findViewById(R.id.Clean);
         Button community = findViewById(R.id.Community);
         Button guide = findViewById(R.id.Guide);
+        ImageButton setting = findViewById(R.id.Settings);
 
-        brew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        brew.setOnClickListener(this);
+        clean.setOnClickListener(this);
+        community.setOnClickListener(this);
+        guide.setOnClickListener(this);
+        setting.setOnClickListener(this);
 
-            }
-        });
-
-        clean.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        community.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        guide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Brew:
+                Toast.makeText(this, "Brew clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Clean:
+                Toast.makeText(this, "Clean clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Community:
+                Toast.makeText(this, "Community clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Guide:
+                Toast.makeText(this, "Guide clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Settings:
+                Toast.makeText(this, "Setting clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
 
+    }
 }
