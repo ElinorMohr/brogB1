c84e854e4cf6c6e00e34aad01817f44ef68392fe
Mee-creator <s195389@student.dtu.dk>
2020-10-26 13:08:50 +0100

    Logo tilføjet.

--- app/src/main/java/org/dtu/brogb1/FirstTime.java
+++ app/src/main/java/org/dtu/brogb1/FirstTime.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.dtu.brogb1.service.Loading;
 
 public class FirstTime extends AppCompatActivity implements View.OnClickListener {
 
     Button bluetooth;
+    TextView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class FirstTime extends AppCompatActivity implements View.OnClickListener
 
         bluetooth = findViewById(R.id.bluetooth);
         bluetooth.setOnClickListener(this);
+
     }
 
     @Override
