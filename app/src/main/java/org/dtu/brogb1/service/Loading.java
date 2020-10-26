0a1b411518d4c51f4d3a53647e35bbeb014ce653
Mee-creator <s195389@student.dtu.dk>
2020-10-26 12:39:49 +0100

    Aktivitet  mellem FirstTime og loading fungerer.

--- app/src/main/java/org/dtu/brogb1/service/Loading.java
+++ app/src/main/java/org/dtu/brogb1/service/Loading.java
@@ -1,6 +1,7 @@
 package org.dtu.brogb1.service;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +9,14 @@ import org.dtu.brogb1.R;
 
 public class Loading extends AppCompatActivity {
 
+    TextView loading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        loading = findViewById(R.id.loading);
+
     }
 }
