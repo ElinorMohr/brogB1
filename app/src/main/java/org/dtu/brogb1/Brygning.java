cc997e94487a4f915c68649fd3160af93387ff0e
Kristoffer <ktis10@hotmail.com>
2020-10-27 13:23:42 +0100

    bryg knappen kommer med at den brygger. Dog loader den ikke

--- app/src/main/java/org/dtu/brogb1/Brygning.java
+++ app/src/main/java/org/dtu/brogb1/Brygning.java
@@ -2,15 +2,39 @@ package org.dtu.brogb1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class Brygning extends AppCompatActivity {
+Button brygNu;
+    private ProgressBar progressBarAnimation;
+    private ObjectAnimator progressAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brygning);
+        brygNu = (Button) findViewById(R.id.BrewNow);
+        BrewingLoading brewingLoading = new BrewingLoading(Brygning.this);
 
+        brygNu.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                brewingLoading.startLoadingDialog();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        brewingLoading.dismissDialog();
+                    }
+                }, 5000);
+            }
+        });
     }
 }
