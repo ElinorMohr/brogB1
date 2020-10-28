7df14f9e79c6eb2d23d1ceae4233ebe19d5b8e79
ElinorMohr <ElinorMohr@users.noreply.github.com>
2020-10-28 11:45:42 +0100

    Opg 15
    Knapper med afrundning

--- app/src/main/java/org/dtu/brogb1/activity/RecipeActivity.java
+++ app/src/main/java/org/dtu/brogb1/activity/RecipeActivity.java
@@ -1,5 +1,6 @@
 package org.dtu.brogb1.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +18,14 @@ public class RecipeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
         edit = findViewById(R.id.edit_recipe);
         brew = findViewById(R.id.brew_recipe);
         image = findViewById(R.id.image_recipe);
         input = findViewById(R.id.input_recipe);
+        Drawable draw = getDrawable(R.drawable.roundedbutton);
+        edit.setBackground(draw);
+        edit.setBackgroundColor(getResources().getColor(R.color.brown_3));
         setContentView(R.layout.activity_recipe);
 
 
