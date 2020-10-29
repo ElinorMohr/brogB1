10e774c9eb7ca17b5c3fe8aec806a14babcccc81
Mee-creator <s195389@student.dtu.dk>
2020-10-29 17:32:57 +0100

    Option klasse oprettet. Overordnede layout lavet.

---
+++ app/src/main/java/org/dtu/brogb1/service/Option.java
@@ -0,0 +1,16 @@
+package org.dtu.brogb1.service;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.dtu.brogb1.R;
+
+public class Option extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+    }
+}
