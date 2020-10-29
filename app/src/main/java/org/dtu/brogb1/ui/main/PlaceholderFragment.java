f371ef382bc60cb93ad934fe2459264a6aa70f89
ElinorMohr <ElinorMohr@users.noreply.github.com>
2020-10-29 17:30:30 +0100

    Opg 8

--- app/src/main/java/org/dtu/brogb1/ui/main/PlaceholderFragment.java
+++ app/src/main/java/org/dtu/brogb1/ui/main/PlaceholderFragment.java
@@ -1,9 +1,14 @@
 package org.dtu.brogb1.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,7 +17,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.dtu.brogb1.MainActivity;
 import org.dtu.brogb1.R;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -21,7 +29,6 @@ public class PlaceholderFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
 
     public static PlaceholderFragment newInstance(int index) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -34,12 +41,6 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
-        int index = 1;
-        if (getArguments() != null) {
-            index = getArguments().getInt(ARG_SECTION_NUMBER);
-        }
-        pageViewModel.setIndex(index);
     }
 
     @Override
@@ -47,11 +48,41 @@ public class PlaceholderFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.opskrifter_layout, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
+        ListView listMain = root.findViewById(R.id.list_view_main);
+        ListView listSec = root.findViewById(R.id.list_view_sec);
+        ArrayList<String> viewMain = new ArrayList<String>();
+        ArrayList<String> viewSes = new ArrayList<String>();
+        viewMain.add("Fars morgen kaffe");
+        viewMain.add("Ekstra mælk i denne");
+        viewSes.add("Latten");
+        int fragmentNum = getArguments().getInt(ARG_SECTION_NUMBER);
+        switch (fragmentNum){
+            case 2:
+                ArrayAdapter<String> adapterSec = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewSes);
+                listSec.setAdapter(adapterSec);
+            case 1:
+                ArrayAdapter<String> adapterMain = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewMain);
+                listMain.setAdapter(adapterMain);
+        }
+
+        listMain.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3)
+            {
+                Intent intent = new Intent(new Intent(getContext(), MainActivity.class));
+                startActivity(intent);
+            }
+        });
+        listSec.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3)
+            {
+                Intent intent = new Intent(new Intent(getContext(), MainActivity.class));
+                startActivity(intent);
             }
         });
         return root;
