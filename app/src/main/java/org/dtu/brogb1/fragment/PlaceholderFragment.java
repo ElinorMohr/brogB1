54f9511345f50a417a6536a2abf4db742d8d10fb
ElinorMohr <ElinorMohr@users.noreply.github.com>
2020-11-23 11:55:31 +0100

    status

--- app/src/main/java/org/dtu/brogb1/fragment/PlaceholderFragment.java
+++ app/src/main/java/org/dtu/brogb1/fragment/PlaceholderFragment.java
@@ -1,81 +1,81 @@
-package org.dtu.brogb1.fragment;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import org.dtu.brogb1.activity.Sections;
-import org.dtu.brogb1.R;
-
-import java.util.ArrayList;
-
-/**
- * A placeholder fragment containing a simple view.
- */
-public class PlaceholderFragment extends Fragment {
-
-    private static final String ARG_SECTION_NUMBER = "section_number";
-
-
-    public static PlaceholderFragment newInstance(int index) {
-        PlaceholderFragment fragment = new PlaceholderFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(ARG_SECTION_NUMBER, index);
-        fragment.setArguments(bundle);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.recipes_layout, container, false);
-        ListView listMain = root.findViewById(R.id.list_view_main);
-        ListView listSec = root.findViewById(R.id.list_view_sec);
-        ArrayList<String> viewMain = new ArrayList<String>();
-        ArrayList<String> viewSes = new ArrayList<String>();
-        viewMain.add("Fars morgen kaffe");
-        viewMain.add("Ekstra mælk i denne");
-        viewSes.add("Latten");
-        int fragmentNum = getArguments().getInt(ARG_SECTION_NUMBER);
-        switch (fragmentNum) {
-            case 2:
-                ArrayAdapter<String> adapterSec = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewSes);
-                listSec.setAdapter(adapterSec);
-            case 1:
-                ArrayAdapter<String> adapterMain = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewMain);
-                listMain.setAdapter(adapterMain);
-        }
-
-        listMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View v, int position,
-                                    long arg3) {
-                Intent intent = new Intent(new Intent(getContext(), Sections.class));
-                startActivity(intent);
-            }
-        });
-        listSec.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View v, int position,
-                                    long arg3) {
-                Intent intent = new Intent(new Intent(getContext(), Sections.class));
-                startActivity(intent);
-            }
-        });
-        return root;
-    }
-}
+//package org.dtu.brogb1.fragment;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.fragment.app.Fragment;
+//
+//import org.dtu.brogb1.activity.Sections;
+//import org.dtu.brogb1.R;
+//
+//import java.util.ArrayList;
+//
+///**
+// * A placeholder fragment containing a simple view.
+// */
+//public class PlaceholderFragment extends Fragment {
+//
+//    private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//
+//    public static PlaceholderFragment newInstance(int index) {
+//        PlaceholderFragment fragment = new PlaceholderFragment();
+//        Bundle bundle = new Bundle();
+//        bundle.putInt(ARG_SECTION_NUMBER, index);
+//        fragment.setArguments(bundle);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Override
+//    public View onCreateView(
+//            @NonNull LayoutInflater inflater, ViewGroup container,
+//            Bundle savedInstanceState) {
+//        View root = inflater.inflate(R.layout.recipes_layout, container, false);
+//        ListView listMain = root.findViewById(R.id.list_view_main);
+//        ListView listSec = root.findViewById(R.id.list_view_sec);
+//        ArrayList<String> viewMain = new ArrayList<String>();
+//        ArrayList<String> viewSes = new ArrayList<String>();
+//        viewMain.add("Fars morgen kaffe");
+//        viewMain.add("Ekstra mælk i denne");
+//        viewSes.add("Latten");
+//        int fragmentNum = getArguments().getInt(ARG_SECTION_NUMBER);
+//        switch (fragmentNum) {
+//            case 2:
+//                ArrayAdapter<String> adapterSec = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewSes);
+//                listSec.setAdapter(adapterSec);
+//            case 1:
+//                ArrayAdapter<String> adapterMain = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewMain);
+//                listMain.setAdapter(adapterMain);
+//        }
+//
+//        listMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapter, View v, int position,
+//                                    long arg3) {
+//                Intent intent = new Intent(new Intent(getContext(), Sections.class));
+//                startActivity(intent);
+//            }
+//        });
+//        listSec.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapter, View v, int position,
+//                                    long arg3) {
+//                Intent intent = new Intent(new Intent(getContext(), Sections.class));
+//                startActivity(intent);
+//            }
+//        });
+//        return root;
+//    }
+//}
--- app/src/main/java/org/dtu/brogb1/fragment/Recipes.java
+++ app/src/main/java/org/dtu/brogb1/fragment/Recipes.java
@@ -1,20 +1,57 @@
 package org.dtu.brogb1.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import org.dtu.brogb1.R;
+import org.dtu.brogb1.activity.Brewing;
+import org.dtu.brogb1.activity.Sections;
+
+import java.util.ArrayList;
 
 public class Recipes extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.recipes_layout, container, false);
+        View root =  inflater.inflate(R.layout.recipes_layout, container, false);
+        ListView listMain = root.findViewById(R.id.list_view_main_recipes);
+        ListView listSec = root.findViewById(R.id.list_view_sec_recipes);
+        ArrayList<String> viewMain = new ArrayList<String>();
+        ArrayList<String> viewSes = new ArrayList<String>();
+        viewMain.add("Fars morgen kaffe");
+        viewMain.add("Ekstra mælk i denne");
+        viewSes.add("Latten");
+        ArrayAdapter<String> adapterSec = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewSes);
+        listSec.setAdapter(adapterSec);
+        ArrayAdapter<String> adapterMain = new ArrayAdapter<String>(getActivity(), R.layout.list_view_layout, viewMain);
+        listMain.setAdapter(adapterMain);
+
+        listMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3) {
+                Intent intent = new Intent(new Intent(getContext(), Brewing.class));
+                startActivity(intent);
+            }
+        });
+        listSec.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3) {
+                Intent intent = new Intent(new Intent(getContext(), Brewing.class));
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 }
