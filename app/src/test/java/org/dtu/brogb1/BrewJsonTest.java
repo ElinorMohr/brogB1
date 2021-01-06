94fc20483ec1bded463eff542a48f1d4a98f2b19
Asim Raja <asim.raja11@gmail.com>
2021-01-06 14:03:22 +0100

    Junit test for Brew

---
+++ app/src/test/java/org/dtu/brogb1/BrewJsonTest.java
@@ -0,0 +1,25 @@
+package org.dtu.brogb1;
+
+import org.dtu.brogb1.model.Brew;
+import org.dtu.brogb1.model.BrewException;
+import org.dtu.brogb1.model.BrewFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BrewJsonTest {
+    @Test
+    public void SaveTest()
+    {
+        Brew brew = new Brew (1,1,1,1,1,
+                1,1,"1","1");
+        try {
+
+            String Json = brew.toJson();
+            Brew brew2 = BrewFactory.fromJson(Json);
+        } catch (BrewException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+}
