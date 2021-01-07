6f539084d84d33f009de35990e8cc63b28684291
Meetopia <s195389@student.dtu.dk>
2021-01-07 19:33:22 +0100

    Oprettet tilhørende klasser til muligheder, og gjort dem "pæne".

--- app/src/main/java/org/dtu/brogb1/activity/Option.java
+++ app/src/main/java/org/dtu/brogb1/activity/Option.java
@@ -1,6 +1,9 @@
 package org.dtu.brogb1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,11 +15,33 @@ import org.dtu.brogb1.R;
  * @author Asim Raja s040727
  */
 
-public class Option extends AppCompatActivity {
+public class Option extends AppCompatActivity implements View.OnClickListener {
+
+    Button language, connections;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
+
+        language = findViewById(R.id.language);
+        connections = findViewById(R.id.connections);
+
+        language.setOnClickListener(this);
+        connections.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == language){
+
+        }
+
+        if (v == connections){
+
+        }
+
+
     }
 }
--- app/src/main/java/org/dtu/brogb1/activity/community/Guide.java
+++ app/src/main/java/org/dtu/brogb1/activity/community/Guide.java
@@ -11,7 +11,7 @@ public class Guide extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_option);
+            setContentView(R.layout.activity_guide);
         }
 }
 
