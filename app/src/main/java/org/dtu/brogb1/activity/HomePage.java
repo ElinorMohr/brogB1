ff8a2962ed77f48cdff33bf4c9ba273d732e9513
ElinorMohr <ElinorMohr@users.noreply.github.com>
2021-01-07 12:54:59 +0100

    Menu

--- app/src/main/java/org/dtu/brogb1/activity/HomePage.java
+++ app/src/main/java/org/dtu/brogb1/activity/HomePage.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -47,10 +50,23 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
-        menu.add(R.string.clean).setOnMenuItemClickListener()
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
-
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option_1:
+                Intent intent = new Intent(this, CleanActivityStep1.class);
+                startActivity(intent);
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public void onClick(View v) {
