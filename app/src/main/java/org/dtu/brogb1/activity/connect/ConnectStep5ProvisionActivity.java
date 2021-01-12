c78ab8757ca87edda6ec793c58ccdc0c0378c6cf
Folkmann <theme1256@gmail.com>
2021-01-12 10:20:56 +0100

    Måske kan esp32 privisioneres nu

--- app/src/main/java/org/dtu/brogb1/activity/connect/ConnectStep5ProvisionActivity.java
+++ app/src/main/java/org/dtu/brogb1/activity/connect/ConnectStep5ProvisionActivity.java
@@ -1,38 +1,286 @@
 package org.dtu.brogb1.activity.connect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.widget.ContentLoadingProgressBar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.espressif.provisioning.DeviceConnectionEvent;
+import com.espressif.provisioning.ESPConstants;
+import com.espressif.provisioning.ESPProvisionManager;
+import com.espressif.provisioning.listeners.ProvisionListener;
 
 import org.dtu.brogb1.R;
 import org.dtu.brogb1.activity.HomePage;
 import org.dtu.brogb1.activity.Loading;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
+/**
+ * Basically en carbon-copy af esp-idf-provisioning-android/ProvisionActivity
+ * https://github.com/espressif/esp-idf-provisioning-android/blob/master/app/src/main/java/com/espressif/ui/activities/ProvisionActivity.java
+ *
+ * @author Theis Villumsen s195461
+ */
 public class ConnectStep5ProvisionActivity extends AppCompatActivity {
+    private static final String TAG = ConnectStep5ProvisionActivity.class.getSimpleName();
+
+    private TextView tvTitle, tvBack, tvCancel;
+    private ImageView tick1, tick2, tick3;
+    private ContentLoadingProgressBar progress1, progress2, progress3;
+    private TextView tvErrAtStep1, tvErrAtStep2, tvErrAtStep3, tvProvError;
+
+    private Button btnOk;
+    private TextView txtOkBtn;
+
+    private String ssidValue, passphraseValue = "";
+    private ESPProvisionManager provisionManager;
+    private boolean isProvisioningCompleted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_step5_provision);
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        Intent intent = new Intent(ConnectStep5ProvisionActivity.this, HomePage.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
-                        startActivity(intent);
-                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                        ConnectStep5ProvisionActivity.this.finish();
-                    }
-                },
-                2000
-        );
+        Intent intent = getIntent();
+        ssidValue = intent.getStringExtra("wifi_name");
+        passphraseValue = intent.getStringExtra("wifi_pass");
+        provisionManager = ESPProvisionManager.getInstance(getApplicationContext());
+        initViews();
+        EventBus.getDefault().register(this);
+
+        Log.d(TAG, "Selected AP -" + ssidValue);
+        showLoading();
+        doProvisioning();
     }
 
     @Override
     public void onBackPressed() {
+        provisionManager.getEspDevice().disconnectDevice();
         super.onBackPressed();
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(DeviceConnectionEvent event) {
+        Log.d(TAG, "On Device Connection Event RECEIVED : " + event.getEventType());
+
+        switch (event.getEventType()) {
+            case ESPConstants.EVENT_DEVICE_DISCONNECTED:
+                if (!isFinishing() && !isProvisioningCompleted) {
+                    showAlertForDeviceDisconnected();
+                }
+                break;
+        }
+    }
+
+    private View.OnClickListener okBtnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            provisionManager.getEspDevice().disconnectDevice();
+            finish();
+        }
+    };
+
+    private void initViews() {
+        tick1 = findViewById(R.id.iv_tick_1);
+        tick2 = findViewById(R.id.iv_tick_2);
+        tick3 = findViewById(R.id.iv_tick_3);
+
+        progress1 = findViewById(R.id.prov_progress_1);
+        progress2 = findViewById(R.id.prov_progress_2);
+        progress3 = findViewById(R.id.prov_progress_3);
+
+        tvErrAtStep1 = findViewById(R.id.tv_prov_error_1);
+        tvErrAtStep2 = findViewById(R.id.tv_prov_error_2);
+        tvErrAtStep3 = findViewById(R.id.tv_prov_error_3);
+        tvProvError = findViewById(R.id.tv_prov_error);
+
+        btnOk = findViewById(R.id.btn_done_prov);
+        btnOk.setOnClickListener(okBtnClickListener);
+    }
+
+    private void doProvisioning() {
+        tick1.setVisibility(View.GONE);
+        progress1.setVisibility(View.VISIBLE);
+
+        provisionManager.getEspDevice().provision(ssidValue, passphraseValue, new ProvisionListener() {
+            @Override
+            public void createSessionFailed(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tick1.setImageResource(R.drawable.ic_error);
+                        tick1.setVisibility(View.VISIBLE);
+                        progress1.setVisibility(View.GONE);
+                        tvErrAtStep1.setVisibility(View.VISIBLE);
+                        tvErrAtStep1.setText(R.string.error_session_creation);
+                        tvProvError.setVisibility(View.VISIBLE);
+                        hideLoading();
+                    }
+                });
+            }
+
+            @Override
+            public void wifiConfigSent() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tick1.setImageResource(R.drawable.ic_checkbox_on);
+                        tick1.setVisibility(View.VISIBLE);
+                        progress1.setVisibility(View.GONE);
+                        tick2.setVisibility(View.GONE);
+                        progress2.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+
+            @Override
+            public void wifiConfigFailed(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tick1.setImageResource(R.drawable.ic_error);
+                        tick1.setVisibility(View.VISIBLE);
+                        progress1.setVisibility(View.GONE);
+                        tvErrAtStep1.setVisibility(View.VISIBLE);
+                        tvErrAtStep1.setText(R.string.error_prov_step_1);
+                        tvProvError.setVisibility(View.VISIBLE);
+                        hideLoading();
+                    }
+                });
+            }
+
+            @Override
+            public void wifiConfigApplied() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tick2.setImageResource(R.drawable.ic_checkbox_on);
+                        tick2.setVisibility(View.VISIBLE);
+                        progress2.setVisibility(View.GONE);
+                        tick3.setVisibility(View.GONE);
+                        progress3.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+
+            @Override
+            public void wifiConfigApplyFailed(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tick2.setImageResource(R.drawable.ic_error);
+                        tick2.setVisibility(View.VISIBLE);
+                        progress2.setVisibility(View.GONE);
+                        tvErrAtStep2.setVisibility(View.VISIBLE);
+                        tvErrAtStep2.setText(R.string.error_prov_step_2);
+                        tvProvError.setVisibility(View.VISIBLE);
+                        hideLoading();
+                    }
+                });
+            }
+
+            @Override
+            public void provisioningFailedFromDevice(final ESPConstants.ProvisionFailureReason failureReason) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (failureReason) {
+                            case AUTH_FAILED:
+                                tvErrAtStep3.setText(R.string.error_authentication_failed);
+                                break;
+                            case NETWORK_NOT_FOUND:
+                                tvErrAtStep3.setText(R.string.error_network_not_found);
+                                break;
+                            case DEVICE_DISCONNECTED:
+                            case UNKNOWN:
+                                tvErrAtStep3.setText(R.string.error_prov_step_3);
+                                break;
+                        }
+                        tick3.setImageResource(R.drawable.ic_error);
+                        tick3.setVisibility(View.VISIBLE);
+                        progress3.setVisibility(View.GONE);
+                        tvErrAtStep3.setVisibility(View.VISIBLE);
+                        tvProvError.setVisibility(View.VISIBLE);
+                        hideLoading();
+                    }
+                });
+            }
+
+            @Override
+            public void deviceProvisioningSuccess() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        isProvisioningCompleted = true;
+                        tick3.setImageResource(R.drawable.ic_checkbox_on);
+                        tick3.setVisibility(View.VISIBLE);
+                        progress3.setVisibility(View.GONE);
+                        hideLoading();
+                    }
+                });
+            }
+
+            @Override
+            public void onProvisioningFailed(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tick3.setImageResource(R.drawable.ic_error);
+                        tick3.setVisibility(View.VISIBLE);
+                        progress3.setVisibility(View.GONE);
+                        tvErrAtStep3.setVisibility(View.VISIBLE);
+                        tvErrAtStep3.setText(R.string.error_prov_step_3);
+                        tvProvError.setVisibility(View.VISIBLE);
+                        hideLoading();
+                    }
+                });
+            }
+        });
+    }
+
+    private void showLoading() {
+        btnOk.setEnabled(false);
+        btnOk.setAlpha(0.5f);
+    }
+
+    public void hideLoading() {
+        btnOk.setEnabled(true);
+        btnOk.setAlpha(1f);
+    }
+
+    private void showAlertForDeviceDisconnected() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        builder.setCancelable(false);
+        builder.setTitle(R.string.error_title);
+        builder.setMessage(R.string.dialog_msg_ble_device_disconnection);
+
+        // Set up the buttons
+        builder.setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+
+        builder.show();
     }
 }
