466d8ec39c253f7bf3a29d346f8c2c3f519b0cf7
ElinorMohr <ElinorMohr@users.noreply.github.com>
2021-01-15 13:33:23 +0100

    match edit og new intent

--- app/src/main/java/org/dtu/brogb1/activity/NewBrew.java
+++ app/src/main/java/org/dtu/brogb1/activity/NewBrew.java
@@ -139,10 +139,12 @@ public class NewBrew extends AppCompatActivity {
             favoriteOn = !favoriteOn;
         });
         coffeeImageView.setOnClickListener(v -> {
+            Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            getIntent.setType("image/");
+
             Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             pickIntent.setType("image/");
-
-            Intent chooserIntent = Intent.createChooser(pickIntent, "Select Image");
+            Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
             chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
 
             startActivityForResult(chooserIntent, 1); //request code til det der sendes videre.
